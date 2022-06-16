8cf7969d6b86701ffbcc4a08f7cb49b0e85d00fe
Shaurya Garg <sgarg@onetrust.com>
2022-06-15 21:43:50 -0400

    Home page completed - remove unused vars

--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -1,5 +1,4 @@
 import { React, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import { TeamTile } from '../components/TeamTile';
 import './HomePage.scss';
 export const HomePage = () => {
