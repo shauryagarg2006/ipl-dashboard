f624cae1721bdcc5f5c2a3dacf5875e6b63a92a2
Shaurya Garg <sgarg@onetrust.com>
2022-06-15 21:40:59 -0400

    Home page completed

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { TeamPage } from './pages/TeamPage';
 import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/teams/:teamName" element={<TeamPage />} />
           <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} />
         </Routes>
